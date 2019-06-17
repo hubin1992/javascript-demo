7e6fba2bb6842bdf5047615dfe602140bd681ec2
hubin1992 <javascripthubin@163.com>
2019-06-17 18:04:23 +0800

    fs

--- node/12.fs/6. 删除文件夹的操作.js
+++ node/12.fs/6. 删除文件夹的操作.js
@@ -23,21 +23,46 @@
 // })
 
 //同步删除非空文件夹的实现方法
-const fs  = require("fs")
+const fs = require("fs")
 const path = require("path")
-function rmdir_D_sycn(dir){
+function rmdir_D_sycn(dir) {
   let dirCont = fs.readdirSync(dir);
-  dirCont.forEach(function(item){
-      let curPath = path.join(dir,item)
-      let child =  fs.statSync(curPath)
-      if(child.isDirectory()){
-        console.log("isDirectory")// 由于是同步执行，所以只有当这个里面所有的文件执行完成了才去执行fs.rmdir
-        rmdir_D_sycn(curPath)
-      }else{
-        fs.unlinkSync(curPath)
-      }
+  dirCont.forEach(function (item) {
+    let curPath = path.join(dir, item)
+    let child = fs.statSync(curPath)
+    if (child.isDirectory()) {
+      console.log("isDirectory")// 由于是同步执行，所以只有当这个里面所有的文件执行完成了才去执行fs.rmdir
+      rmdir_D_sycn(curPath)
+    } else {
+      fs.unlinkSync(curPath)
+    }
   })
   console.log("rmdirSync")
   fs.rmdirSync(dir);//最后三次一起执行
 }
-rmdir_D_sycn("a")
+// rmdir_D_sycn("a")
+
+//异步方式删除非空文件夹
+//思路：
+//1. 我们首先要转换成state类，来判断他是文件和文件夹，如果是文件我们直接删掉，如果是文件夹我们在继续递归调用自用。
+function rmdir_D(dir) {//写了三版的递归压根不好处理，只能考虑用promise来试一下
+  return new Promise((resolve, reject) => {
+
+    fs.stat(dir, function (err, stats) {//我理解的是将整个文件夹转换成这个stat类
+      if (err) return reject(err)
+      if (stats.isDirectory()) {
+        fs.readdir(dir, function (err, files) {
+          if (err) return reject(err)
+          Promise.all(files.map((item) => {// [b,1.html]
+            rmdir_D(path.join(dir, item)).then(() => {//当所有的都删除完成后，在去删除自己
+              fs.rmdir(dir, resolve)
+            });
+          }))
+        })
+      } else {
+        fs.unlink(dir, resolve)
+      }
+    })
+  })
+}
+rmdir_D("c3")
---
+++ node/13.path/1.js
@@ -0,0 +1,46 @@
+//path 中常用的api
+
+const path = require("path");
+// console.log(path)
+
+//path.json() 路径拼接
+//__dirname
+//basename
+//extname
+//parse
+//sep
+//delimiter
+//resolve
+
+//1. 路径拼接
+console.log(path.join("a","1.html")) ;//"a/1.html"
+//2. __dirname 显示绝对路径
+console.log(__dirname);///Users/hubin/learn-file/js/node/13.path 显示的是绝对路径
+//3. basename 显示名字
+console.log(path.basename("1.jpg")) //1.jpg 显示的就是名字
+
+//4. extname 显示扩展名字
+console.log(path.extname("1.jpg")) //.jpg
+
+//5. parse(string)
+console.log(path.parse(__dirname)) ;
+/* 
+{ root: '/',
+  dir: '/Users/hubin/learn-file/js/node',
+  base: '13.path',
+  ext: '.path',
+  name: '13' }
+*/
+
+//6. path.format(obj) 这个obj是固定的方式{root,dir,base}
+console.log(path.format({
+  root:"/root",
+  dir:"a/b/c",
+  base:"2.txt"
+}));//a/b/c/2.txt像这样的默认是省略root
+
+//path.sep() ;//提供平台特定的路径片段分隔符：  Windows 上是 \    POSIX 上是 /
+//path.delimiter() ;// 提供平台特定的路径定界符 ;是window   :是POSIX
+
+//7.path.reslove()  -->个人感觉用的较少  path.resolve() 方法将路径或路径片段的序列解析为绝对路径。
+console.log(path.resolve())
